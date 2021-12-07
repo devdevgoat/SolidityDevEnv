da54aefa75104735d55129350b4ce19c54454bba
Russ <russell.w.lamb@gmail.com>
2021-12-07 09:18:49 -0500

    added dotenv and a sample file. added network fork

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,5 @@
 require("@nomiclabs/hardhat-waffle");
+require('dotenv').config();
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -18,4 +19,12 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  */
 module.exports = {
   solidity: "0.8.4",
+  networks: {
+    hardhat: {
+      forking: {
+        url: "https://eth-mainnet.alchemyapi.io/v2/"+process.env.ALCHEMY_KEY,
+        blockNumber: 13747841
+      }
+    }
+  }
 };
